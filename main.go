ec3addc5c2c4e53d2a1278ff5b575ac1fd045aa1
catatsuy <catatsuy@catatsuy.org>
2015-05-29 11:14:28 +0900

    main.goに集約

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"os"
 )
 
+func byteWrite(f *os.File, str []byte) {
+	if _, err := f.Write(append(str, '\n')); err != nil {
+		panic(err)
+	}
+}
+
+func stringWrite(f *os.File, str []byte) {
+	if _, err := f.WriteString(string(str) + "\n"); err != nil {
+		panic(err)
+	}
+}
+
 func bufioWrite(w *bufio.Writer, f *os.File, str []byte) {
 	defer w.Flush()
 	if w.Available() <= len(str) {
