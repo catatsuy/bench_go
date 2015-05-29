a7714cb49f8d29d9b9ec350c2d9c0ebe91c7578f
catatsuy <catatsuy@catatsuy.org>
2015-05-29 11:11:56 +0900

    use testing

---
+++ bench_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func BenchmarkByteWrite(b *testing.B) {
+	str := []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+	fileName := "tmp.log"
+	ferr := os.Remove(fileName)
+	if ferr != nil {
+		if !os.IsNotExist(ferr) {
+			panic(ferr.Error())
+		}
+	}
+	f, ferr := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer f.Close()
+	if ferr != nil {
+		panic(ferr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		byteWrite(f, str)
+	}
+}
+
+func BenchmarkStringWrite(b *testing.B) {
+	str := []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+	fileName := "tmp.log"
+	ferr := os.Remove(fileName)
+	if ferr != nil {
+		if !os.IsNotExist(ferr) {
+			panic(ferr.Error())
+		}
+	}
+	f, ferr := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer f.Close()
+	if ferr != nil {
+		panic(ferr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		stringWrite(f, str)
+	}
+}
+
+func BenchmarkBufioWrite(b *testing.B) {
+	str := []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+	fileName := "tmp.log"
+	ferr := os.Remove(fileName)
+	if ferr != nil {
+		if !os.IsNotExist(ferr) {
+			panic(ferr.Error())
+		}
+	}
+	f, ferr := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer f.Close()
+	if ferr != nil {
+		panic(ferr.Error())
+	}
+	w := bufio.NewWriterSize(f, 8192)
+	for i := 0; i < b.N; i++ {
+		bufioWrite(w, f, str)
+	}
+}
+
+func BenchmarkDirectWrite(b *testing.B) {
+	str := []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+	fileName := "tmp.log"
+	ferr := os.Remove(fileName)
+	if ferr != nil {
+		if !os.IsNotExist(ferr) {
+			panic(ferr.Error())
+		}
+	}
+	f, ferr := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer f.Close()
+	if ferr != nil {
+		panic(ferr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		directWrite(f, str)
+	}
+}
--- fileWrite.go
+++ fileWrite.go
@@ -5,49 +5,19 @@ import (
 	"os"
 )
 
-const (
-	WRITE_COUNT = 10000000
-)
-
-func main() {
-	str := []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-
-	fileName := "tmp.log"
-	ferr := os.Remove(fileName)
-	if ferr != nil {
-		if !os.IsNotExist(ferr) {
-			panic(ferr.Error())
-		}
-	}
-	f, ferr := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	if ferr != nil {
-		panic(ferr.Error())
-	}
-
-	bufioWrite(f, str)
-	// directWrite(f, str)
-}
-
-func bufioWrite(f *os.File, str []byte) {
-	defer f.Close()
-	w := bufio.NewWriterSize(f, 8192/2)
+func bufioWrite(w *bufio.Writer, f *os.File, str []byte) {
 	defer w.Flush()
-	for i := 0; i < WRITE_COUNT; i++ {
-		if w.Available() <= len(str) {
-			w.Flush()
-		}
-		if _, err := w.Write(append(str, '\n')); err != nil {
-			panic(err)
-		}
+	if w.Available() <= len(str) {
 		w.Flush()
 	}
+	if _, err := w.Write(append(str, '\n')); err != nil {
+		panic(err)
+	}
+	w.Flush()
 }
 
 func directWrite(f *os.File, str []byte) {
-	defer f.Close()
-	for i := 0; i < WRITE_COUNT; i++ {
-		if _, err := f.Write(append(str, '\n')); err != nil {
-			panic(err)
-		}
+	if _, err := f.Write(append(str, '\n')); err != nil {
+		panic(err)
 	}
 }
